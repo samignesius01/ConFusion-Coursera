502b7803d7caf0994625308d623d1fdbb4cfe5b2
sam <samignesh1998@gmail.com>
2020-07-22 23:03:21 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {baseURL} from './shared/baseurl';
 
 import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 
@@ -57,7 +58,8 @@ import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
