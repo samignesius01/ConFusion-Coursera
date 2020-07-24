52b3becdad3ef07a5f304021dc4875eaf3d743d7
sam <samignesh1998@gmail.com>
2020-07-24 11:06:46 +0530

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,10 +5,20 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { ActivatedRoute } from '@angular/router';
 
+import {flyInOut,expand} from '../animations/app.animation';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host:{
+    '[@flyInOut]':'true',
+    'style':'display:block;'
+  },
+  animations:[
+    flyInOut(),
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import {DishService} from '../services/dish.service';
 import {Promotion} from '../shared/promotion';
 import {PromotionService} from '../services/promotion.service';
 
+import {flyInOut,expand} from '../animations/app.animation';
+
 
 import {Leader} from '../shared/lead';
 import {LEADER} from '../shared/leader';
@@ -13,7 +15,15 @@ import {LeaderService} from '../services/leader.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host:{
+    '[@flyInOut]':'true',
+    'style':'display:block;'
+  },
+  animations:[
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
 
